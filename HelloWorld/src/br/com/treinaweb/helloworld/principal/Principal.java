b958a525130e8160b19b3ce9005e81d52298efa7
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-25 14:42:57 -0300

    Hello World

---
+++ HelloWorld/src/br/com/treinaweb/helloworld/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.helloworld.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Comentário de uma linha, inicia com duas barras
+		System.out.println("Hello World");
+		/*
+		Comentário em bloco
+		Aceita múltiplas linhas
+		*/
+	}
+}
