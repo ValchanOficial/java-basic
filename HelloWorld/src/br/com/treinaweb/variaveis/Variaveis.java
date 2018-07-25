26b3f75d3c7b459750ba1121164305e1dde19090
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-25 15:03:54 -0300

    Variaveis

---
+++ HelloWorld/src/br/com/treinaweb/variaveis/Variaveis.java
@@ -0,0 +1,14 @@
+package br.com.treinaweb.variaveis;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		String nomeVariavel = "TreinaWeb";
+		int variavelInteiro = 2018;
+		double variavelPontoFlutuante = 20.18;
+		
+		System.out.println("Ola, "+nomeVariavel);
+		System.out.println("Estamos em "+variavelInteiro+"!");
+		System.out.println(variavelPontoFlutuante);
+	}
+}
