3bb25b7af813a8672d01315f05db5445106c4e72
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-25 15:59:54 -0300

    Convertendo tipos

--- HelloWorld/src/br/com/treinaweb/variaveis/Variaveis.java
+++ HelloWorld/src/br/com/treinaweb/variaveis/Variaveis.java
@@ -1,5 +1,7 @@
 package br.com.treinaweb.variaveis;
 
+import java.util.Scanner;
+
 public class Variaveis {
 
 	public static void main(String[] args) {
@@ -10,5 +12,24 @@ public class Variaveis {
 		System.out.println("Ola, "+nomeVariavel);
 		System.out.println("Estamos em "+variavelInteiro+"!");
 		System.out.println(variavelPontoFlutuante);
+		
+		//Convertendo o tipo de dado
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite a sua idade: ");
+		String idadeUsuario = leitor.nextLine(); //recebe o dado do tipo String no console 
+		//String idadeUsuario = leitor.nextInt();
+		
+		//Estrutura de decisão + conversão de String para Integer
+		if(Integer.parseInt(idadeUsuario)>=18) {
+			System.out.println("Usuário maior de idade.");
+		}else {
+			System.out.println("Usuário menor de idade.");
+		}
+		
+		double a = 1.5; //double é maior que int
+		int b = (int)a; //convertendo para int
+		System.out.println(b);
+		
+		leitor.close();
 	}
 }
