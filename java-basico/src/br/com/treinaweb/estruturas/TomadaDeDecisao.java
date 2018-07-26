5409754018e19545aa1096ce09233cea3f7b5c96
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-26 17:17:40 -0300

    Estrutura IF ELSE

--- java-basico/src/br/com/treinaweb/estruturas/TomadaDeDecisao.java
+++ java-basico/src/br/com/treinaweb/estruturas/TomadaDeDecisao.java
@@ -6,11 +6,26 @@ public class TomadaDeDecisao {
 
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
+		Scanner leitorString = new Scanner(System.in);
+		System.out.println("Digite a sua idade: ");
+		int idade = leitor.nextInt();
+		System.out.println("Digite (M) se você é homem ou (F) se você é mulher: ");
+		String sexo = leitorString.nextLine();
 		
-		if(true) {
-			
+		if(sexo.equals("M")) {
+			if(idade>=21) {
+				System.out.println("Você não vai mais crescer!");
+			}else {
+				System.out.println("Você vai mais crescer mais!");
+			}
+		}else if(sexo.equals("F")){
+			if(idade>=18) {
+				System.out.println("Você não vai mais crescer!");
+			}else {
+				System.out.println("Você vai mais crescer mais!");
+			}
 		}else {
-			
+			System.out.println("Opção inválida!");
 		}
 	}
 }
