30266221116385acb78fd1c37a8043560c141257
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-28 21:50:37 -0300

    Adicionado encapsulamento

--- poo/src/br/com/treinaweb/app/Programa.java
+++ poo/src/br/com/treinaweb/app/Programa.java
@@ -7,20 +7,18 @@ public class Programa {
 	public static void main(String[] args) {
 		Classe cachorro = new Classe(); //chamando classe
 		//setando atributos
-		cachorro.nome = "Totó";
-		cachorro.idade = 5;
-		cachorro.especie = "Cachorro";
-		//executando ações(métodos)
-		cachorro.dormir();
-		cachorro.comer();
-		System.out.println(cachorro.crescer());
-		
-		Classe gato = new Classe();
-		gato.nome = "Frajola";
-		gato.idade = 3;
-		gato.especie = "Gato";
-		gato.comer();
-		gato.dormir();
-		System.out.println(gato.crescer());
+		try {
+			cachorro.setNome("Totó");
+			cachorro.setIdade(5);
+			cachorro.setEspecie("Cachorro");
+			System.out.println(cachorro.isEstaVivo());
+			//executando ações(métodos)
+			cachorro.dormir();
+			cachorro.comer();
+			System.out.println(cachorro.crescer());
+			cachorro.morrer();//estaVivo = false
+		} catch (Exception e) {
+		System.out.println(e.getMessage());;
+		}
 	}
 }
--- poo/src/br/com/treinaweb/classe/Classe.java
+++ poo/src/br/com/treinaweb/classe/Classe.java
@@ -4,10 +4,36 @@ package br.com.treinaweb.classe;
 public class Classe {
 	
 	//Atributos
-	public String nome;
-	public int idade;
-	public String especie;
+	private String nome;
+	private int idade;
+	private String especie;
+	private boolean estaVivo = true;
 	
+	//Encapsulamento
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) throws Exception {
+		if(nome.equals("")) {
+			throw new Exception("O nome não pode ser vazio.");
+		}
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public boolean isEstaVivo() {
+		return estaVivo;
+	}
 	//Métodos
 	public String crescer() {
 		return nome +" está crescendo..";
@@ -17,5 +43,9 @@ public class Classe {
 	}
 	public void comer() {
 		System.out.println(nome+" está comendo..");
+	}
+	public void morrer() {
+		estaVivo = false;
+		System.out.println(nome+" infelizmente morreu.");
 	}
 }
